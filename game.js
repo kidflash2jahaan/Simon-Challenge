c47a8ede27e75a3e6ae6cc57ec2d3ec38d1599fe
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-06-04 16:42:02 -0700

    Step 3

--- game.js
+++ game.js
@@ -4,6 +4,11 @@ let gamePattern = []
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
-    
     gamePattern.push(randomChosenColor)
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100)
+
+    new Audio("sounds/" + randomChosenColor + ".mp3").play()
 }
+
+nextSequence()
