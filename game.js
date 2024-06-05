5f79008d43d18679f2d23e335fa71867b9d9d75b
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-06-04 19:53:07 -0700

    Step 4

--- game.js
+++ game.js
@@ -1,14 +1,24 @@
 let buttonColors = ["red", "blue", "green", "yellow"]
 let gamePattern = []
+let userClickPattern = []
+
+function playAnimation(color) {
+    $("#" + color).fadeOut(100).fadeIn(100)
+    new Audio("sounds/" + color + ".mp3").play()
+}
 
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
-    let randomChosenColor = buttonColors[randomNumber];
+    let randomChosenColor = buttonColors[randomNumber]
     gamePattern.push(randomChosenColor)
-
-    $("#" + randomChosenColor).fadeOut(100).fadeIn(100)
-
-    new Audio("sounds/" + randomChosenColor + ".mp3").play()
+    
+    playAnimation(randomChosenColor)
 }
+
+$(".btn").on("click", function (event) {
+    let userChosenColor = event.target.id
+    
+    userClickPattern.push(userChosenColor)
+})
 
 nextSequence()
