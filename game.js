8623bc29f810b21fd1020579a7394573c725da74
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-06-05 06:48:33 -0700

    Step 7

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ let buttonColors = ["red", "blue", "green", "yellow"]
 let gamePattern = []
 let userClickPattern = []
 
+let level = 0
+
 function playAnimation(color, clicked = false) {
     if (clicked === false) $("#" + color).fadeOut(100).fadeIn(100)
     else {
@@ -14,18 +16,26 @@ function playAnimation(color, clicked = false) {
 }
 
 function nextSequence() {
+    level++
+    $("#level-title").text("Level " + level)
+
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber]
     gamePattern.push(randomChosenColor)
 
-    playAnimation(randomChosenColor)
+    setTimeout(function () {
+        playAnimation(randomChosenColor)
+    }, 1000)
 }
+
+$(document).on("keypress", function () {
+    if (level === 0) nextSequence()
+})
 
 $(".btn").on("click", function (event) {
     let userChosenColor = event.target.id
     userClickPattern.push(userChosenColor)
 
     playAnimation(userChosenColor, true)
+    nextSequence()
 })
-
-nextSequence()
